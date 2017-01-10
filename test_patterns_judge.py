5d67010f260ed95959b004741e6ca016f2dfadf0
lyn716 <965606089@qq.com>
2017-01-10 16:29:42 +0800

    update gitnore

---
+++ test_patterns_judge.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+"""
+@file:      test_patterns_judge
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.3
+@editor:    PyCharm Mac
+@create:    2017/1/10 16:27
+@description:
+            --
+"""
