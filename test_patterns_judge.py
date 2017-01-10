5567fc6dca6fb2458e71145284f7f3c2dc71ef33
lyn716 <965606089@qq.com>
2017-01-10 23:36:33 +0800

    update test

--- test_patterns_judge.py
+++ test_patterns_judge.py
@@ -18,14 +18,13 @@ cards = GameCards().to_arr()
 for card in cards:
     print(card)
 '''
-
 cot = 0
 rf = 0
 sf = 0
 ff = 0
 while(1):
     cot += 1
-    time.sleep(0)
+    time.sleep(0.01)
     random_five_cards = []
 
     for i in range(5):
@@ -41,12 +40,11 @@ while(1):
     #print(random_five_cards)
 
     for pattern in [
-        StraightFlush, RoyalFlush, #Flush, Straight, FullHouse,
-        FourOfOneKind, #Set, TwoPairs, OnePair , HighCard
+        RoyalFlush, StraightFlush, FourOfOneKind,
+        FullHouse, Flush, Straight,
+        Set, TwoPairs, OnePair, HighCard
     ]:
         test_model = pattern(random_five_cards)
-        #print(test_model.value_map)
-        #print(test_model.value_set)
         if test_model.judge_result:
             if pattern==FourOfOneKind:
                 ff += 1
@@ -54,17 +52,23 @@ while(1):
                     cot, test_model.name,
                     random_five_cards,ff
                 ))
-            if pattern==StraightFlush:
+            elif pattern==StraightFlush:
                 sf += 1
                 print('[{}]\t {}:\t{}\t{}'.format(
                     cot, test_model.name,
                     random_five_cards, sf
                 ))
-            if pattern==RoyalFlush:
+            elif pattern==RoyalFlush:
                 rf += 1
                 print('-----------------------------------------------------')
                 print('[{}]\t {}:\t{}\t{}'.format(
                     cot, test_model.name,
                     random_five_cards, rf
                 ))
+            else:
+                print('[{}]\t {}:\t{}\t'.format(
+                    cot, test_model.name,
+                    random_five_cards
+                ))
+                break
     #print('------')
