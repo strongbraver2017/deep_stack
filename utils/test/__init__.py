e6b656bd1e200bfd2b9e31ec750bee43f3127450
lyn716 <tonylu716@gmail.com>
2017-05-16 12:03:56 +0800

    alter directory

---
+++ utils/test/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@file:      __init__.py.py
+@author:    lyn
+@contact:   tonylu716@gmail.com
+@python:    3.5
+@editor:    Vim
+@create:    5/16/17 12:01 PM
+@description:
+        
+"""
